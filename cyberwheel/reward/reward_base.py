f0a42f88cadad6e348bf34bbb86633a3f0b83c24
jianzhnie <jianzhnie@126.com>
2024-09-14 09:39:44 +0800

    add cyberwheel rewards

--- cyberwheel/reward/reward_base.py
+++ cyberwheel/reward/reward_base.py
@@ -1,30 +1,28 @@
-from abc import abstractmethod
-from typing import Dict, NewType, Tuple
+from abc import ABC, abstractmethod
+from typing import Dict, NewType, Tuple, Union
 
-RewardMap = NewType('RewardMap', Dict[str, Tuple[int | float, int | float]])
+RewardMap = NewType("RewardMap", Dict[str, Tuple[Union[int, float], Union[int, float]]])
 
 
-def calc_quadratic(x: float, a=0.0, b=0.0, c=0.0) -> float:
+def calc_quadratic(x: float, a: float = 0.0, b: float = 0.0, c: float = 0.0) -> float:
     return a * x**2 + b * x + c
 
 
 class RecurringAction:
-
     def __init__(self, id: str, action: str) -> None:
         self.id = id
         self.action = action
 
 
-class Reward:
-
-    def __init__(self, red_rewards, blue_rewards) -> None:
+class Reward(ABC):
+    def __init__(self, red_rewards: RewardMap, blue_rewards: RewardMap) -> None:
         self.red_rewards = red_rewards
         self.blue_rewards = blue_rewards
 
     @abstractmethod
-    def calculate_reward(self) -> int | float:
-        raise NotImplementedError
+    def calculate_reward(self) -> Union[int, float]:
+        pass
 
     @abstractmethod
     def reset(self) -> None:
-        raise NotImplementedError
+        pass
--- cyberwheel/reward/step_detected_reward.py
+++ cyberwheel/reward/step_detected_reward.py
@@ -3,11 +3,12 @@ from cyberwheel.reward.reward_base import RewardMap
 
 
 class StepDetectedReward(RecurringReward):
+    """这段代码定义了一个名为StepDetectedReward的类， 用于计算蓝队(防御方)在检测到红队(攻击方)行动时的奖励."""
 
     def __init__(
         self,
         blue_rewards: RewardMap,
-        max_steps,
+        max_steps: int,
     ) -> None:
         """Increases the reward the earlier that the red agent is detected. So
         the best reward it can get is one in which the blue agent immediately
@@ -18,28 +19,39 @@ class StepDetectedReward(RecurringReward):
         Reward Function: max_steps / n, where n is the number of steps
         """
         self.reward_function = max_steps * 10
-        self.step_detected = 99999999
+        self.min_decoys = 1
+        self.decoy_penalty = -100
+        self.initial_step = float('inf')
+
         super().__init__(
             red_rewards={},
             blue_rewards=blue_rewards,
         )
 
-    def calculate_reward(
-        self,
-        red_action_alerted: bool,
-        step_detected: int,
-    ) -> int | float:
-        step_detected_reward = 0
+    def calculate_reward(self, red_action_alerted: bool,
+                         step_detected: int) -> float:
+        """
+        - 如果检测到红队行动且当前步骤小于之前记录的步骤,更新step_detected并计算奖励。
+        - 如果蓝队没有部署至少一个防御措施,则给予 -100 的惩罚。
+        - 最后加上蓝队的循环行动奖励。
+
+        Args:
+            red_action_alerted (bool): 是否检测到红队行动
+            step_detected (int): 当前步骤
+
+        Returns:
+            float: 奖励
+        """
+        reward = 0
         if red_action_alerted and step_detected < self.step_detected:
             self.step_detected = step_detected
-            step_detected_reward = self.reward_function / self.step_detected
+            reward += self.reward_function / self.step_detected
 
-        b = 0
-        if len(self.blue_recurring_actions
-               ) < 1:  # Should deploy at least 1 decoy
-            b = -100
-        return step_detected_reward + b + self.sum_recurring_blue()
+        # Should deploy at least 1 decoy
+        if len(self.blue_recurring_actions) < self.min_decoys:
+            reward += self.decoy_penalty
+        return reward + self.sum_recurring_blue()
 
     def reset(self, ) -> None:
-        self.step_detected = 999999999
+        self.step_detected = self.initial_step
         self.blue_recurring_actions = []
